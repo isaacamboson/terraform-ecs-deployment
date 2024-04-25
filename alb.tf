ec3184219315b2474fbba09e656b49eff7236f4d
Isaac Amboson <isaacamboson@gmail.com>
2024-04-24 23:27:56 -0400

    pushing Jenkinsfile

--- alb.tf
+++ alb.tf
@@ -25,11 +25,11 @@ resource "aws_lb_listener" "clixx-app" {
 }
 
 resource "aws_lb_target_group" "clixx-app-tg" {
-  name            = "${local.ApplicationPrefix}-app-tg"
-  port            = "80"
-  protocol        = "HTTP"
-  target_type     = "ip"
-  vpc_id          = aws_vpc.vpc_main.id
+  name                 = "${local.ApplicationPrefix}-app-tg"
+  port                 = "80"
+  protocol             = "HTTP"
+  target_type          = "ip"
+  vpc_id               = aws_vpc.vpc_main.id
   deregistration_delay = 120
 
   health_check {
