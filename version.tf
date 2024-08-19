24584ade88a0c9a6d8899b6a86fe330607fc85f5
Isaac Amboson <isaacamboson@gmail.com>
2024-08-19 09:47:44 -0400

    changing the terraform version to a compartible version with jenkins

--- version.tf
+++ version.tf
@@ -1,21 +1,21 @@
-# #configuring terraform version and AWS provider 
-# terraform {
-#   required_version = ">= 0.12"
-#   required_providers {
-#     aws = {
-#       source  = "hashicorp/aws"
-#       version = "~> 3.0"
-#     }
-#   }
-# }
-
 #configuring terraform version and AWS provider 
 terraform {
-  required_version = ">= 1.8.4"
+  required_version = ">= 0.12"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.16"
+      version = "~> 3.0"
     }
   }
 }
+
+# #configuring terraform version and AWS provider 
+# terraform {
+#   required_version = ">= 1.8.4"
+#   required_providers {
+#     aws = {
+#       source  = "hashicorp/aws"
+#       version = "~> 4.16"
+#     }
+#   }
+# }
